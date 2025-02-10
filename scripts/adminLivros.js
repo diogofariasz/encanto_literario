8297ec49888e9d96acee1f39143f05b9f51e5452
adrian5g <jogadorsquarebr@gmail.com>
2025-02-10 16:52:05 -0300

    listagem de livro adicionada

--- scripts/adminLivros.js
+++ scripts/adminLivros.js
@@ -1,25 +1,27 @@
 // Verifica se a função criarElemento está definida
 
 // Seleciona o elemento contêiner
-const livrosContainerElement = document.querySelector(".livros-container");
+const livrosContainerElement = document.querySelector('tbody');
 
 // Verifica se o contêiner existe antes de adicionar elementos a ele
 if (livrosContainerElement) {
-    booksData.forEach((bookData) => {
-        const bookElement = criarElemento('tr', {
-            children: [
-                criarElemento('td', { text: bookData.id }),
-                criarElemento('td', { text: bookData.title }),
-                criarElemento('td', { text: formatarDinheiro(bookData.price) }),
-                criarElemento('td', { text: bookData.image_url }),
-                criarElemento('td', { text: bookData.author.name }),
-                criarElemento('td', { text: bookData.category }),
-                criarElemento('td', { text: bookData.description }),
-            ],
-        });
-
-        livrosContainerElement.appendChild(bookElement);
+  booksData.forEach((bookData) => {
+    const bookElement = criarElemento('tr', {
+      children: [
+        criarElemento('td', { text: bookData.id }),
+        criarElemento('td', { text: bookData.title }),
+        criarElemento('td', { text: formatarDinheiro(bookData.price) }),
+        criarElemento('td', { text: bookData.image_url }),
+        criarElemento('td', { text: bookData.author.name }),
+        criarElemento('td', { text: bookData.category }),
+        criarElemento('td', { text: bookData.desc }),
+      ],
     });
+
+    livrosContainerElement.appendChild(bookElement);
+
+    bookElement.addEventListener('click', () => {})
+  });
 } else {
-    console.error('Elemento .livros-container não encontrado');
+  console.error('Elemento .livros-container não encontrado');
 }
